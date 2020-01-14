4a05a652cbb85e125121022daa44bae7487d856e
Jhevison <jhevison74@gmail.com>
2020-01-14 01:06:43 -0300

    Add files via upload

---
+++ confi.sh
@@ -0,0 +1,16 @@
+mv confi.sh /$USER/$HOME/
+rm -rf confi
+apt update
+
+
+cd /etc/ && touch hostname && echo "localhost" >> hostname
+
+echo "cd /$USER/$HOME/" >> /$USER/$HOME/.bashrc
+echo "export USER=root" >> /$USER/$HOME/.bashrc
+
+apt install xfonts-base -y
+apt install tightvncserver -y
+apt install lxde --no-install-recommends lightdm- -y
+apt install eclipse-wtp -y
+
+
